0359c49412d239b2918ea55f18aa2240d32f5d8b
Mohamed Hamza Falih <falih.hamza95@gmail.com>
2023-05-24 17:16:48 +0200

    Modifying instructions

--- version.php
+++ version.php
@@ -27,8 +27,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020110501;
+$plugin->version   = 2020110502;
 $plugin->requires  = 2020110900;
 $plugin->component = 'mod_offlinequiz';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "v3.10.1";
+$plugin->release   = "v3.10.2";
